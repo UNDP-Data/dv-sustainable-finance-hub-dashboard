c65a5ea27c1f5a3d9fc9afe65510529e5abeeb5e
Anastasia Balagurova <73748770+balagurova@users.noreply.github.com>
2024-11-13 18:48:08 +0200

    Add stat cards and map

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import AppMap from './AppMap';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const getEl = (embedSelector: string) => {
+  if (typeof embedSelector === 'string') {
+    const el = document.querySelector(embedSelector);
+    if (!el) {
+      // eslint-disable-next-line no-console
+      console.error(`No div matching selector "${embedSelector}"`);
+      return null;
+    }
+    return el;
+  }
+  return embedSelector;
+};
+
+const vizApp = getEl('[data-viz-app-map]');
+if (vizApp) {
+  const rootEmbed = ReactDOM.createRoot(vizApp);
+  rootEmbed.render(
+    <React.StrictMode>
+      <AppMap />
+    </React.StrictMode>,
+  );
+}
